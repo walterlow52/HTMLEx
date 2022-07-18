5f43356da80e71510d7820621eafab693a519022
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2022-07-17 20:35:50 -0400

    Update format.js

--- format.js
+++ format.js
@@ -1,1 +1,13 @@
-
+// HTML Geolocation API
+var locate = document.getElementById("location");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+  else {
+    locate.innerHTML = "Sorry, the geolocation API currently doesn't work on your browser."; 
+  }
+}
+function showPosition(position) {
+  locate.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude; 
+}
