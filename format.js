2641cf437d09f476af6b8bc196ba8e570020f419
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2022-07-17 20:47:50 -0400

    Update format.js

--- format.js
+++ format.js
@@ -11,3 +11,19 @@ function getLocation() {
 function showPosition(position) {
   locate.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude; 
 }
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      locate.innerHTML = "The request for the Geolocation API has been denied."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      locate.innerHTML = "The location of the user is unavailable."
+      break;
+    case error.TIMEOUT:
+      locate.innerHTML = "The request for the Geolocation API has timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      locate.innerHTML = "Sorry, an unknown error has occurred."
+      break;
+  }
+}
